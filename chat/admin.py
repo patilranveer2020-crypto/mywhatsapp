9c904faf07b6ee39432c01012afa4f0ed2ba2134
Ranveer Patil <ranveer@example.com>
2026-02-26 14:39:39 +0530

    Show Push in admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Profile
-from .models import ChatGroup, GroupMessage , UserStatus
+from .models import ChatGroup, GroupMessage , UserStatus, Message, PushSubscription
 
 admin.site.register(ChatGroup)
 admin.site.register(GroupMessage)
 
 admin.site.register(Profile)
 admin.site.register(UserStatus)
+admin.site.register(PushSubscription)
 # Register your models here.
