e04d0893c3967e41d7d73b7a5743af9d892eb3c8
Ranveer Patil <ranveer@example.com>
2026-03-11 10:49:08 +0530

    WebRTC Queue Fix

--- mywhatsapp/settings.py
+++ mywhatsapp/settings.py
@@ -93,17 +93,18 @@ ASGI_APPLICATION = 'mywhatsapp.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 """
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 # --- ADD NEW POSTGRESQL ---
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+#DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
